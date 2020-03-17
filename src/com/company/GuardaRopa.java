734afb08f860919db79fc00f108aa1870c7f13b4
Juani Unzurrunzaga <juanunzu@gmail.com>
2020-03-17 11:52:27 -0300

    por corregir

---
+++ src/com/company/GuardaRopa.java
@@ -0,0 +1,53 @@
+package com.company;
+
+import java.sql.SQLOutput;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class GuardaRopa {
+
+  private Integer identificador;
+  private Map<Integer, List<Prenda>> listaDePrendas;
+
+  public GuardaRopa() {
+    this.identificador = 0;
+    this.listaDePrendas = new HashMap<Integer, List<Prenda>>();
+  }
+
+  public void mostrarPrendas(){
+    for (Integer integer : listaDePrendas.keySet()) {
+      System.out.println(listaDePrendas.get(integer));
+    }
+  }
+
+  public void mostrarPrendasPorIdentificador(Integer identificador){
+    System.out.println(this.listaDePrendas.get(identificador));
+  }
+
+  public Integer guardarPrendas(List<Prenda> listaDePrenda){
+    this.identificador++;
+    this.listaDePrendas.put(this.identificador, listaDePrenda);
+    System.out.println("Se han guardado sus prendas. Su número de guardado es " + this.identificador);
+    return this.identificador;
+  }
+
+  public List<Prenda> devolverPrendas(Integer identificador){
+
+    //se guarda en una variable local lo que devuelve el get de la lista general con el identificador pasado como parametro
+    List<Prenda> prendasSolicitadas = this.listaDePrendas.get(identificador);
+    //se saca de la lista general las prendas que devuelven el get con el identificador
+    this.listaDePrendas.remove(prendasSolicitadas);
+    //se devuelven los resultados
+    System.out.println("Se devuelve la lista de prendas correspondiente al numero de guardado " + identificador);
+    return prendasSolicitadas;
+
+  }
+
+  public Integer getIdentificador() {
+    return identificador;
+  }
+
+
+
+}
---
+++ src/com/company/Main.java
@@ -0,0 +1,49 @@
+package com.company;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Prenda camperaDeJuan = new Prenda("uniqlo", "tal");
+        Prenda camperaDeMeli = new Prenda("rapsodia", "tal otra");
+        Prenda mochilaDeJuan = new Prenda("acme", "negra");
+        Prenda carteraDeMeli = new Prenda("cartera", "acme");
+        Prenda camperaDeImanol = new Prenda("adidas", "acme");
+        Prenda mochilaDeImanol = new Prenda("adidas", "negra");
+        Prenda mochilaDeLautaro = new Prenda("nike", "negra");
+
+        List<Prenda> prendasJuanMeli = new ArrayList<Prenda>();
+        List<Prenda> prendasImaLau = new ArrayList<Prenda>();
+
+        prendasJuanMeli.add(camperaDeJuan);
+        prendasJuanMeli.add(camperaDeMeli);
+        prendasJuanMeli.add(mochilaDeJuan);
+        prendasJuanMeli.add(carteraDeMeli);
+
+        prendasImaLau.add(camperaDeImanol);
+        prendasImaLau.add(mochilaDeImanol);
+        prendasImaLau.add(mochilaDeLautaro);
+
+        GuardaRopa teatroArgentino = new GuardaRopa();
+
+        teatroArgentino.guardarPrendas(prendasJuanMeli);
+
+        teatroArgentino.guardarPrendas(prendasImaLau);
+
+        teatroArgentino.devolverPrendas(1);
+
+        teatroArgentino.devolverPrendas(1);
+
+        System.out.println(teatroArgentino.getIdentificador());
+
+        teatroArgentino.mostrarPrendasPorIdentificador(0);
+
+
+
+
+    }
+}
